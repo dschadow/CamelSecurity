279a1e46561c2ac01492d32cb85a55c9b4abdf8b
Dominik Schadow <dominikschadow@googlemail.com>
2012-02-24 11:50:01 +0100

    Added JUnit dependency and dummy test
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

---
+++ CamelPayloadSecurity/src/test/java/com/trivadis/camel/security/FindUserDataEnpointTest.java
@@ -0,0 +1,14 @@
+package com.trivadis.camel.security;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindUserDataEnpointTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
