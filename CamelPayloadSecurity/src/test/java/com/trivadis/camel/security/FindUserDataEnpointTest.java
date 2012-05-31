f7d2dbd004dfd24913121d86b76bf1b92eab7d28
Dominik Schadow <dominikschadow@googlemail.com>
2012-05-31 10:31:10 +0200

    Completed JUnit tests for all methods
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- CamelPayloadSecurity/src/test/java/com/trivadis/camel/security/FindUserDataEnpointTest.java
+++ CamelPayloadSecurity/src/test/java/com/trivadis/camel/security/FindUserDataEnpointTest.java
@@ -1,28 +1,76 @@
 package com.trivadis.camel.security;
 
 import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.trivadis.camel.security.user.UserData;
+
+/**
+ * JUnit tests for all secured routes.
+ *
+ * @author Dominik Schadow, Trivadis GmbH
+ * @version 1.0.0
+ */
 public class FindUserDataEnpointTest extends CamelSpringTestSupport {
+    private static final String USERDATA_COMPLETE =
+            "Trivadis GmbH, Dominik, Schadow, Industriestraße 4, 70565, Stuttgart, Germany, 1234567890, 49";
+    private static final String USERDATA_PARTIAL =
+            "Trivadis GmbH, Dominik, Schadow, Industriestraße 4, 70565, Stuttgart, Germany, 1234567890, 0";
+    private UserData userData = new UserData();
+    
+    @Before
+    public void setup() {
+        userData.setCompany("Trivadis GmbH");
+        userData.setFirstName("Dominik");
+        userData.setLastName("Schadow");
+        userData.setStreet("Industriestraße 4");
+        userData.setZip("70565");
+        userData.setCity("Stuttgart");
+        userData.setCountry("Germany");
+        userData.setSocialSecurityNumber(1234567890);
+    }
+    
     @Test
     public void testSymmetricEncryption() {
-        String responseAsBase64 = template.requestBody("direct:findUserDataSymEnc", 1234567890, String.class);
-        assertNotNull(responseAsBase64);
-        assertTrue(responseAsBase64.endsWith("="));
+        String responseEncrypted = template.requestBody("direct:findUserDataSymEnc", userData, String.class);
+        assertNotNull(responseEncrypted);
+        assertFalse(responseEncrypted.contains(USERDATA_COMPLETE));
+        assertFalse(responseEncrypted.contains(USERDATA_PARTIAL));
+        assertTrue(responseEncrypted.endsWith("="));
     }
     
     @Test
     public void testAsymmetricEncryption() {
-        String responseAsBase64 = template.requestBody("direct:findUserDataAsymEnc", 1234567890, String.class);
-        assertNotNull(responseAsBase64);
+        String responseEncrypted = template.requestBody("direct:findUserDataAsymEnc", userData, String.class);
+        assertNotNull(responseEncrypted);
+        assertFalse(responseEncrypted.contains(USERDATA_COMPLETE));
+        assertFalse(responseEncrypted.contains(USERDATA_PARTIAL));
+    }
+    
+    @Test
+    public void testSymmetricXMLEncryption() {
+        String responseEncrypted = template.requestBody("direct:findUserDataSymEncXML", userData, String.class);
+        assertNotNull(responseEncrypted);
+        assertFalse(responseEncrypted.contains(USERDATA_COMPLETE));
+        assertFalse(responseEncrypted.contains(USERDATA_PARTIAL));
+    }
+    
+    @Test
+    public void testAsymmetricXMLEncryption() {
+        String responseEncrypted = template.requestBody("direct:findUserDataAsymEncXML", userData, String.class);
+        assertNotNull(responseEncrypted);
+        assertFalse(responseEncrypted.contains(USERDATA_COMPLETE));
+        assertFalse(responseEncrypted.contains(USERDATA_PARTIAL));
     }
     
     @Test
     public void testSignature() {
-        int response = template.requestBody("direct:findUserDataSign", 1234567890, Integer.class);
-        assertEquals(1234567890, response);
+        String response = template.requestBody("direct:findUserDataSign", userData, String.class);
+        assertNotNull(response);
+        assertEquals(USERDATA_PARTIAL, response);
     }
 
     @Override
