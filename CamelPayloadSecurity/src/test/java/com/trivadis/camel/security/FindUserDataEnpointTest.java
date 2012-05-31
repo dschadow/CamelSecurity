2683a037681fcbd917c64a96088659402c8a670f
Dominik Schadow <dominikschadow@googlemail.com>
2012-05-31 10:12:12 +0200

    Added JUnit tests
    Signed-off-by: Dominik Schadow <dominikschadow@googlemail.com>

--- CamelPayloadSecurity/src/test/java/com/trivadis/camel/security/FindUserDataEnpointTest.java
+++ CamelPayloadSecurity/src/test/java/com/trivadis/camel/security/FindUserDataEnpointTest.java
@@ -1,14 +1,32 @@
 package com.trivadis.camel.security;
 
-import static org.junit.Assert.*;
-
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
 import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class FindUserDataEnpointTest {
+public class FindUserDataEnpointTest extends CamelSpringTestSupport {
+    @Test
+    public void testSymmetricEncryption() {
+        String responseAsBase64 = template.requestBody("direct:findUserDataSymEnc", 1234567890, String.class);
+        assertNotNull(responseAsBase64);
+        assertTrue(responseAsBase64.endsWith("="));
+    }
+    
+    @Test
+    public void testAsymmetricEncryption() {
+        String responseAsBase64 = template.requestBody("direct:findUserDataAsymEnc", 1234567890, String.class);
+        assertNotNull(responseAsBase64);
+    }
+    
+    @Test
+    public void testSignature() {
+        int response = template.requestBody("direct:findUserDataSign", 1234567890, Integer.class);
+        assertEquals(1234567890, response);
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring/camel-context.xml");
+    }
 }
